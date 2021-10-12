20cfe58d498819a671bc3869a345fdcb9b6fe5a6
undefined undefined <srinivas.rao@dksh.com>
2021-10-12 09:33:51 +0000

    Hot Fix on Filter Search:STRY0013183

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -477,8 +477,10 @@ sap.ui.define([
 			}
 			this.valueHelpId = oEvent.getSource().getId();
 			this.vhFilter = "";
-			if (oUserAccessModel.getData()[sAccess]) {
-				var aValue = oUserAccessModel.getData()[sAccess].split("@");
+			var sIAccess = oUserAccessModel.getData()[sAccess];
+			if (sIAccess) {
+				if(sIAccess.length>0 && sIAccess!=="*"){
+				var aValue = sIAccess.split("@");
 				// retrieve for blank code
 				aValue.push("");
 				this.vhFilter = new Filter({
@@ -487,6 +489,7 @@ sap.ui.define([
 					}),
 					and: false
 				});
+				}
 			}
 			if (aItemVH.includes(sFragment)) {
 				var oItemLevel = oEvent.getSource().getParent().getParent();
